ba3142e7f72517861387371346a77ff12e4c5d31
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-02-24 15:22:50 +0200

    Add basic file structure

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+/**
+ * Start of Global Variables.
+ */
+
+
+/**
+ * End of Global Variables.
+ *
+ * Start of Helper Functions.
+ */
+
+
+/**
+ * End of Helper Functions.
+ *
+ * Start of Main Functions.
+ */
+
+
+/**
+ * End of Main Functions.
+ *
+ * Start of Event Listeners.
+ */
+
+
+/**
+ * End of Event Listeners.
+ */
